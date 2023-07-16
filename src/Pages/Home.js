caf76f26836396dc7246d03ca9a3b7cc6a5054dd
Pankaj Doodhwal <pankajdoodhwal0084395@gmail.com>
2023-07-17 01:42:49 +0530

    services part in wrapper-2

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -87,6 +87,51 @@ const Home = () => {
           </div>
         </div>
       </section>
+      <section className="home-wrapper-2 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="services d-flex align-items-center justify-content-between">
+                <div className="d-flex align-items-center gap-10">
+                  <img src="images/service.png" alt="service" />
+                  <div>
+                    <h6>Free shipping</h6>
+                    <p className="mb-0">From all orders over $100</p>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center gap-10">
+                  <img src="images/service-02.png" alt="service" />
+                  <div>
+                    <h6>Daily Surprice Offers</h6>
+                    <p className="mb-0">Save up to 25% off</p>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center gap-10">
+                  <img src="images/service-03.png" alt="service" />
+                  <div>
+                    <h6>Support 24/7</h6>
+                    <p className="mb-0">Shop with an expert</p>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center gap-10">
+                  <img src="images/service-04.png" alt="service" />
+                  <div>
+                    <h6>Affordable Prices</h6>
+                    <p className="mb-0">Get Factory Direct Price</p>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center gap-10">
+                  <img src="images/service-05.png" alt="service" />
+                  <div>
+                    <h6>Secure Payments</h6>
+                    <p className="mb-0">100% Protected Payments</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
       
     </>
   );
